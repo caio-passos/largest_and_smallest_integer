a105e4933928a7746068499bf7e21eb0aca68813
CaioP <caiopss99@gmail.com>
2023-01-08 01:01:44 -0300

    V.1.2.1 C version of the Code

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <string.h>
+
+int values = NULL;
+
+int main(){
+
+printf("Enter a list of numbers and in the end press enter to finish: \n");
+scanf("%d",&values);
+
+return 0;
+}

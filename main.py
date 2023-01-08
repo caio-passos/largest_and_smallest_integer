d395c4a38d4d88dd699e05fbf04850528b37fcfb
CaioP <caiopss99@gmail.com>
2023-01-07 22:50:11 -0300

    V1.10: Printing a list of integers and displaying the biggest and the smallest value on this list.
    -Now using the build-in function instead of the previous loop, to make it faster.

--- main.py
+++ main.py
@@ -8,13 +8,7 @@ while True:
         print("The list is:", values)
         break
 
-biggest = None  # initializing values as None(empty to
-smallest = None
-
-for x in values:
-    if biggest is None or x > biggest:
-        biggest = x
-    if smallest is None or x < smallest:
-        smallest = x
+biggest = min(values)  # initializing values as None(empty to
+smallest = max(values)
 
 print("The biggest value is:", biggest, "\nthe smallest value is: ", smallest)
